a3982e417ec334ad66cd2e02a00c8ea1b35368ab
Денис Сергеев <denis.sergeev@vsegda-da.com>
2022-06-29 00:05:44 +0300

    Fixes by review

--- src/test/java/ru/netology/service/SalesStatisticTestSet.java
+++ src/test/java/ru/netology/service/SalesStatisticTestSet.java
@@ -8,100 +8,122 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SalesStatisticTestSet {
 
     @Test
-    public void checkMaxSales(){
+    public void checkMaxSales() {
 
-        int[] sales = { 2, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 2 };
-        int sumAllSales = SaleStatisticService.sumAllSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
+
+        int[] sales = {2, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 2};
+        int sumAllSales = saleStatisticService.sumAllSales(sales);
 
         assertEquals(257, sumAllSales, "Сумма продаж " + sumAllSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkAverageSalesAmount(){
+    public void checkAverageSalesAmount() {
 
-        int[] sales = { 2, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 2 };
-        int averageSalesAmount = SaleStatisticService.averageSalesAmount(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(21,  averageSalesAmount, "Средняя сумма продаж " + averageSalesAmount + " посчитана не правильно !");
+        int[] sales = {2, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 2};
+        int averageSalesAmount = saleStatisticService.averageSalesAmount(sales);
+
+        assertEquals(21, averageSalesAmount, "Средняя сумма продаж " + averageSalesAmount + " посчитана не правильно !");
     }
 
     @Test
-    public void checkAverageSalesAmountIfItEquals(){
+    public void checkAverageSalesAmountIfItEquals() {
 
-        int[] sales = { 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50 };
-        int averageSalesAmount = SaleStatisticService.averageSalesAmount(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(50,  averageSalesAmount, "Средняя сумма продаж " + averageSalesAmount + " посчитана не правильно !");
+        int[] sales = {50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50};
+        int averageSalesAmount = saleStatisticService.averageSalesAmount(sales);
+
+        assertEquals(50, averageSalesAmount, "Средняя сумма продаж " + averageSalesAmount + " посчитана не правильно !");
     }
 
     @Test
-    public void checkFirstMonthWithMinSales(){
+    public void checkFirstMonthWithMinSales() {
 
-        int[] sales = { 2, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 3 };
-        int monthWithMinSales = SaleStatisticService.numberMonthWithMinSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(1,  monthWithMinSales, "Месяц с минимальными продажами " + monthWithMinSales + " посчитана не правильно !");
+        int[] sales = {2, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 3};
+        int monthWithMinSales = saleStatisticService.numberMonthWithMinSales(sales);
+
+        assertEquals(1, monthWithMinSales, "Месяц с минимальными продажами " + monthWithMinSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkMiddleMonthWithMinSales(){
+    public void checkMiddleMonthWithMinSales() {
 
-        int[] sales = { 5, 50, 13, 35, 14, 10, 1, 15, 10, 35, 50, 3 };
-        int monthWithMinSales = SaleStatisticService.numberMonthWithMinSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(7,  monthWithMinSales, "Месяц с минимальными продажами " + monthWithMinSales + " посчитана не правильно !");
+        int[] sales = {5, 50, 13, 35, 14, 10, 1, 15, 10, 35, 50, 3};
+        int monthWithMinSales = saleStatisticService.numberMonthWithMinSales(sales);
+
+        assertEquals(7, monthWithMinSales, "Месяц с минимальными продажами " + monthWithMinSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkLastMonthWithMinSales(){
+    public void checkLastMonthWithMinSales() {
 
-        int[] sales = { 5, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 3 };
-        int monthWithMinSales = SaleStatisticService.numberMonthWithMinSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(12,  monthWithMinSales, "Месяц с минимальными продажами " + monthWithMinSales + " посчитана не правильно !");
+        int[] sales = {5, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 3};
+        int monthWithMinSales = saleStatisticService.numberMonthWithMinSales(sales);
+
+        assertEquals(12, monthWithMinSales, "Месяц с минимальными продажами " + monthWithMinSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkFirstMonthWithMaxSales(){
+    public void checkFirstMonthWithMaxSales() {
 
-        int[] sales = { 100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 3 };
-        int monthWithMaxSales = SaleStatisticService.numberMonthWithMaxSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(1,  monthWithMaxSales, "Месяц с максимальными продажами " + monthWithMaxSales + " посчитана не правильно !");
+        int[] sales = {100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 3};
+        int monthWithMaxSales = saleStatisticService.numberMonthWithMaxSales(sales);
+
+        assertEquals(1, monthWithMaxSales, "Месяц с максимальными продажами " + monthWithMaxSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkMiddleMonthWithMaxSales(){
+    public void checkMiddleMonthWithMaxSales() {
 
-        int[] sales = { 100, 50, 13, 35, 14, 200, 21, 15, 10, 35, 50, 150 };
-        int monthWithMaxSales = SaleStatisticService.numberMonthWithMaxSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(6,  monthWithMaxSales, "Месяц с максимальными продажами " + monthWithMaxSales + " посчитана не правильно !");
+        int[] sales = {100, 50, 13, 35, 14, 200, 21, 15, 10, 35, 50, 150};
+        int monthWithMaxSales = saleStatisticService.numberMonthWithMaxSales(sales);
+
+        assertEquals(6, monthWithMaxSales, "Месяц с максимальными продажами " + monthWithMaxSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkLastMonthWithMaxSales(){
+    public void checkLastMonthWithMaxSales() {
 
-        int[] sales = { 100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 150 };
-        int monthWithMaxSales = SaleStatisticService.numberMonthWithMaxSales(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
 
-        assertEquals(12,  monthWithMaxSales, "Месяц с максимальными продажами " + monthWithMaxSales + " посчитана не правильно !");
+        int[] sales = {100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 150};
+        int monthWithMaxSales = saleStatisticService.numberMonthWithMaxSales(sales);
+
+        assertEquals(12, monthWithMaxSales, "Месяц с максимальными продажами " + monthWithMaxSales + " посчитана не правильно !");
     }
 
     @Test
-    public void checkCountMonthWithLessAverage(){
+    public void checkCountMonthWithLessAverage() {
 
-        int[] sales = { 100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 150 };
-        int countMonthWithSalesLessAverage = SaleStatisticService.countMonthWithSalesLessAverage(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
+
+        int[] sales = {100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 150};
+        int countMonthWithSalesLessAverage = saleStatisticService.countMonthWithSalesLessAverage(sales);
 
         assertEquals(8, countMonthWithSalesLessAverage, "Число месяцев с продажами ниже среднего " + countMonthWithSalesLessAverage + " посчитана не правильно !");
     }
 
     @Test
-    public void checkCountMonthWithGreatAverage(){
+    public void checkCountMonthWithGreatAverage() {
 
-        int[] sales = { 100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 150 };
-        int countMonthWithSalesGreatAverage = SaleStatisticService.countMonthWithSalesGrateAverage(sales);
+        SaleStatisticService saleStatisticService = new SaleStatisticService();
+
+        int[] sales = {100, 50, 13, 35, 14, 10, 21, 15, 10, 35, 50, 150};
+        int countMonthWithSalesGreatAverage = saleStatisticService.countMonthWithSalesGrateAverage(sales);
 
         assertEquals(4, countMonthWithSalesGreatAverage, "Число месяцев с продажами выше среднего " + countMonthWithSalesGreatAverage + " посчитана не правильно !");
     }
